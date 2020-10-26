e86beceb4c3589154f713397d87b946da6091603
Louis Varley <Louis.Varley@landscapeinstitute.org>
2020-10-26 22:23:20 +0000

    tweaks

--- web/static/main.js
+++ web/static/main.js
@@ -1,17 +1,18 @@
 
+
+jQuery(document).ready(function(){
+	$("select").initialize( function(){
+		$(this).selectpicker();
+	})
+});
+
+
 edit_add_pin = function(pin = "new"){
 	
 	$.get( "edit?id=" + pin, function( data ) {
 		$( "#get_modal .modal-body" ).html( data );
+		
 		$( "#get_modal").modal('show');
 	});
-	
-}
-
-get_modal_save = function(){
-
-	var i = $(this).prev('form');
-	alert($(i).attr('action'))
-	
-	
+		
 }
